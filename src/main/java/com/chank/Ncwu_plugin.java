875ae4c2288d2cae4cfd23cc6797abb1be956281
Chank <chank616@outlook.com>
2024-05-11 21:59:38 +0800

    初始化提交...

---
+++ src/main/java/com/chank/Ncwu_plugin.java
@@ -0,0 +1,17 @@
+package com.chank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ncwu_plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("你好，华水小水宝！");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
